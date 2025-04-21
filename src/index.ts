3449d1efc9eb55eb784c734115bc3b2b38760a68
Dieguito368 <bautistadiego368@gmail.com>
2025-04-21 07:24:47 -0600

    Cambios

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express  from "express";
 import http from "http";
+import dotenv from "dotenv";
 import { Server } from "socket.io";
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST"]
     }
 });
